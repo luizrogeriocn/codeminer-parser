7fb98b5becf3532f04e84b19c3fab996e92c989b
Luiz Rogério <luizrogeriocn@gmail.com>
2015-07-17 15:37:25 -0300

    Create get_games function to get info from games.log

--- parser.js
+++ parser.js
@@ -6,3 +6,28 @@
 //get clientInfo:   ClientUserinfoChanged(.*)\n
 //get clientIds:    ClientConnect:\s(\d*)\n
 //get clientName:   n\\(.*)\\t\\0
+
+var fs = require('fs');
+var text = fs.readFileSync('games.log', 'utf8');
+
+var get_games = function(text_param){
+  var re = /InitGame(((.|\n)(?!-{60}))*)/g;
+  var s = text_param;
+  var m;
+  var games = [];
+  
+  do {
+      m = re.exec(s);
+      if (m) {
+        var game = {};
+        game.content = m[1];
+        games.push(game);
+      }
+  } while (m);
+  return games;
+};
+
+
+var result = get_games(text);
+var games_js = JSON.stringify(result);
+console.log(games_js);
