6a20f506119a6993b744753bbb0673f5c22c11ed
Luiz Rogério <luizrogeriocn@gmail.com>
2015-07-18 03:29:24 -0300

    Implement a function to get kill stats of a game

--- parser.js
+++ parser.js
@@ -1,43 +1,72 @@
-//get games:        InitGame(((.|\n)(?!-{60}))*)
-//get kills:        Kill:(.*)\n
-//get killer:       \d:\s(.*)\skilled
-//get killed:       killed\s(.*)\sby
-//get meanOfDeath:  by\s(.*)\n
-//get clientInfo:   ClientUserinfoChanged(.*)\n
-//get clientIds:    ClientConnect:\s(\d*)\n
-//get clientName:   n\\(.*)\\t\\0
-
 var fs = require('fs');
 var text = fs.readFileSync('games.log', 'utf8');
 
+
+var get_attribute = function(text_param, re_param){
+  var m;
+  do {
+      m = re_param.exec(text_param);
+      if (m) {
+        return m[1];
+      }
+  } while (m);
+};
+
 var get_kills = function(game_param){
   var re = /Kill:(.*)\n/g;
+  var killer_re = /Kill:\s(\d+)\s/;
+  var killed_re = /\d\s(\d+)\s/;
+  var mean_re = /\d\s(\d+):/;
   var s = game_param;
   var m;
-  var kills = 0;
+  var kills = {};
+  kills.killers = {};
+  kills.total = 0;
+  do {
+      m = re.exec(s);
+      if (m) {
+        kills.total++;
+        var killer = get_attribute(m[0], killer_re);
+        var killed = get_attribute(m[0], killed_re);
+        var mean = get_attribute(m[0], mean_re);
+        if (kills.killers[killer] != undefined)
+          kills.killers[killer] = kills.killers[killer]+1;
+        else
+          kills.killers[killer] = 1;
+      }
+  } while (m);
+  return kills;
+};
+
+var get_players = function(game_param){
+  var re = /ClientUserinfoChanged(.*)\n/g;
+  var s = game_param;
+  var m;
+  var players = {};
   
   do {
       m = re.exec(s);
       if (m) {
-        kills++
+        var id = get_attribute(m[1], /:\s(\d*).*/g);
+        var name = get_attribute(m[1], /n\\(.*)\\t\\\d/g);
+        players[id] = name;
       }
   } while (m);
-  console.log(kills);
-  return kills;
+  return players;
 };
 
 var get_games = function(text_param){
   var re = /InitGame(((.|\n)(?!-{60}))*)/g;
-  var s = text_param;
   var m;
   var games = [];
   
   do {
-      m = re.exec(s);
+      m = re.exec(text_param);
       if (m) {
         var game = {};
-        game.content = m[1];
-        game.total_kills = get_kills(game.content);
+        var content = m[1];
+        game.kills = get_kills(content);
+        game.players = get_players(content);
         games.push(game);
       }
   } while (m);
