ec43ccd0d106fa0a2848d16c4c9865fbfd7cdc1e
Luiz Rogério <luizrogeriocn@gmail.com>
2015-07-19 11:41:53 -0300

    Implement setter methods

--- Game.js
+++ Game.js
@@ -7,6 +7,19 @@ var Game = function (players_info, means_of_death) {
   this.means_of_death = means_of_death;
   this.players_info = players_info;
 
-  
+  this.set_total_kills = function(param){
+    this.total_kills = param['total'];
+  };
+
+  this.set_players = function(){
+    for (var key in this.players_info) {
+      if (this.players_info.hasOwnProperty(key)) {
+        this.players.push(this.players_info[key]);
+      }
+    };
+  };
+
+  // this.set_kills = function(param){};
+  // this.set_kills_by_means = function(kills_param, means_of_death_param){};
 };
 module.exports = Game;
