8dd31e1bebe5da3ba56bf7b874fcbf7a0541076d
Luiz Rogério <luizrogeriocn@gmail.com>
2015-07-19 12:12:59 -0300

    Create a script to run the parser

--- test.js
+++ test.js
@@ -1,13 +1,7 @@
 var fs = require('fs');
-var Game = require('./Game');
+var GameParser = require('./GameParser');
 var text = fs.readFileSync('games.log', 'utf8');
 
-
-var game = new Game();
-games = game.get_games(text);
-console.log(JSON.stringify(games));
-// var result = get_games(text);
-// var games_js = JSON.stringify(result);
-// console.log(games_js);
-// console.log('*************');
-// console.log(JSON.stringify(get_means_of_death(text)));
+var gameParser = new GameParser();
+games = gameParser.parse_games(text);
+console.log(games);
